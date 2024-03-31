12b40f1857ba6340ca7363dd999ce5cda5e23a9a
Dikendev <diken.dev@gmail.com>
2024-03-30 22:41:30 -0300

    feat add route to files / module

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,37 @@
-import { Controller, Get } from '@nestjs/common';
+import {
+  Controller,
+  Post,
+  UploadedFile,
+  UploadedFiles,
+  UseInterceptors,
+} from '@nestjs/common';
 import { AppService } from './app.service';
+import { FileInterceptor, FilesInterceptor } from '@nestjs/platform-express';
+import { pdfToText } from 'pdf-ts';
+import { SheetRepository } from './repository/sheet.repository';
 
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) {}
+  constructor(
+    private readonly sheetRepository: SheetRepository,
+    private readonly appService: AppService,
+  ) {}
 
-  @Get()
-  getHello(): string {
-    return this.appService.getHello();
+  @Post('upload-pdf')
+  @UseInterceptors(FileInterceptor('file'))
+  async pdf(@UploadedFile() file: Express.Multer.File) {
+    return pdfToText(file.buffer);
+  }
+
+  @Post('upload-sheet')
+  @UseInterceptors(FileInterceptor('file'))
+  async csv(@UploadedFile() file: Express.Multer.File) {
+    return this.sheetRepository.read(file.buffer);
+  }
+
+  @Post('ocr')
+  @UseInterceptors(FilesInterceptor('files'))
+  async ocr(@UploadedFiles() files: Array<Express.Multer.File>) {
+    return this.appService.recognizeFile(files);
   }
 }
--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,12 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ExcelModule } from './external/excel/excel.module';
+import { SuccessReadFileModule } from './main/use-case/when-success-read-image/success-read-file.module';
+import { TesseractModule } from './external/ocr/tesseract.module';
 
 @Module({
-  imports: [],
+  imports: [ExcelModule, SuccessReadFileModule, TesseractModule],
   controllers: [AppController],
   providers: [AppService],
 })
