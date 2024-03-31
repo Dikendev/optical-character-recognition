927775afd12277c7cad6d43c5861cbb9f4731eef
Dikendev <diken.dev@gmail.com>
2024-03-30 22:41:04 -0300

    feat add recognize method to handle files

--- src/app.service.ts
+++ src/app.service.ts
@@ -1,8 +1,14 @@
 import { Injectable } from '@nestjs/common';
+import { FileResponse, Tesseract } from './external/ocr/tesseract';
 
 @Injectable()
 export class AppService {
-  getHello(): string {
-    return 'Hello World!';
+  constructor(private readonly tesseract: Tesseract) {}
+
+  async recognizeFile(
+    files: Array<Express.Multer.File>,
+  ): Promise<FileResponse> {
+    const result = await this.tesseract.recognizeFilePt(files);
+    return result;
   }
 }
