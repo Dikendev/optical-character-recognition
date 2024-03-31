1e584723d05ee2e17060e122c1676d9c990a37b0
Dikendev <diken.dev@gmail.com>
2024-03-31 12:00:36 -0300

    feat add query parameter to ocr route

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,14 +1,27 @@
 import {
   Controller,
   Post,
+  Query,
   UploadedFile,
   UploadedFiles,
   UseInterceptors,
+  UsePipes,
+  ValidationPipe,
 } from '@nestjs/common';
 import { AppService } from './app.service';
 import { FileInterceptor, FilesInterceptor } from '@nestjs/platform-express';
 import { pdfToText } from 'pdf-ts';
 import { SheetRepository } from './repository/sheet.repository';
+import { CandidateStatusList } from './main/use-case/when-words-is-found/words-is-found';
+import { IsArray, IsString } from 'class-validator';
+import { Transform } from 'class-transformer';
+
+export class OcrQueryDto {
+  @IsArray()
+  @Transform(({ value }) => JSON.parse(value), { toClassOnly: true })
+  @IsString({ each: true })
+  wordsToFind: string[];
+}
 
 @Controller()
 export class AppController {
@@ -31,7 +44,10 @@ export class AppController {
 
   @Post('ocr')
   @UseInterceptors(FilesInterceptor('files'))
-  async ocr(@UploadedFiles() files: Array<Express.Multer.File>) {
-    return this.appService.recognizeFile(files);
+  async ocr(
+    @UploadedFiles() files: Array<Express.Multer.File>,
+    @Query() wordsToFind: OcrQueryDto,
+  ): Promise<CandidateStatusList> {
+    return this.appService.recognizeFile(files, wordsToFind);
   }
 }
