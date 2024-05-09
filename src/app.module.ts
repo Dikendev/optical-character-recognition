a27d26e0a4c7aad0db4fec5872923b654dc0707d
Dikendev <diken.dev@gmail.com>
2024-05-08 23:42:41 -0300

    feat add pdf module

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,9 +4,10 @@ import { AppService } from './app.service';
 import { ExcelModule } from './external/excel/excel.module';
 import { SuccessReadFileModule } from './main/use-case/when-success-read-image/success-read-file.module';
 import { TesseractModule } from './external/ocr/tesseract.module';
+import { PdfModule } from './external/pdf/pdf.module';
 
 @Module({
-  imports: [ExcelModule, SuccessReadFileModule, TesseractModule],
+  imports: [ExcelModule, SuccessReadFileModule, TesseractModule, PdfModule],
   controllers: [AppController],
   providers: [AppService],
 })
