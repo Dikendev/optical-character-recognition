140fdabbab4dd6f615eb09203405e00e45a64dea
Dikendev <diken.dev@gmail.com>
2024-05-14 22:16:42 -0300

    feat add health route / update ocr dto

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,7 +1,8 @@
 import {
+  Body,
   Controller,
+  Get,
   Post,
-  Query,
   UploadedFile,
   UploadedFiles,
   UseInterceptors,
@@ -9,18 +10,17 @@ import {
 import { AppService } from './app.service';
 import { FileInterceptor, FilesInterceptor } from '@nestjs/platform-express';
 import { SheetRepository } from './repository/sheet.repository';
-import { IsArray, IsString } from 'class-validator';
+import { IsString } from 'class-validator';
 import { Transform } from 'class-transformer';
 import { CandidateStatusList } from './main/use-case/when-words-is-found/interfaces/candidate-status.interface';
 
-export class OcrQueryDto {
-  @IsArray()
-  @Transform(({ value }) => JSON.parse(value), { toClassOnly: true })
+export class OcrDto {
+  @Transform(({ value }) => JSON.parse(value))
   @IsString({ each: true })
   wordsToFind: string[];
 }
 
-@Controller()
+@Controller('ocr')
 export class AppController {
   constructor(
     private readonly sheetRepository: SheetRepository,
@@ -33,12 +33,19 @@ export class AppController {
     return this.sheetRepository.read(file.buffer);
   }
 
-  @Post('ocr')
+  @Post('recognize-words')
   @UseInterceptors(FilesInterceptor('files'))
   async ocr(
     @UploadedFiles() files: Array<Express.Multer.File>,
-    @Query() wordsToFind: OcrQueryDto,
+    @Body() wordsToFind: OcrDto,
   ): Promise<CandidateStatusList> {
     return this.appService.recognizeFile(files, wordsToFind);
+  }
+
+  @Get('health')
+  health() {
+    return {
+      status: 'UP',
+    };
   }
 }
