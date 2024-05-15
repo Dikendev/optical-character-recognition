0cb664bc3a38b20f4b706b489843ac5b9e9267f3
Dikendev <diken.dev@gmail.com>
2024-05-14 22:17:45 -0300

    feat enable cors / set global prefix

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,10 @@ import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors();
+  app.setGlobalPrefix('api');
   app.useGlobalPipes(new ValidationPipe({ transform: true }));
+  console.log('Server is running on http://localhost:3000/api');
   await app.listen(3000);
 }
 bootstrap();
