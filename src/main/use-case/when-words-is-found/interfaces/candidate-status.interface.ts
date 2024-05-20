61760d28fd5a6e2aa9d1a29db7a51aed12f3d0c5
Dikendev <diken.dev@gmail.com>
2024-05-20 20:37:01 -0300

    update Candidate status

--- src/main/use-case/when-words-is-found/interfaces/candidate-status.interface.ts
+++ src/main/use-case/when-words-is-found/interfaces/candidate-status.interface.ts
@@ -8,8 +8,10 @@ export type Status = (typeof Status)[keyof typeof Status];
 
 export interface CandidateStatus {
   fileName: string;
-  documentWords: string;
+  documentWords: string[];
+  searchWords: string[];
   wordsCount: number;
+  foundWords: string[];
   percent: number;
   status: Status;
 }
