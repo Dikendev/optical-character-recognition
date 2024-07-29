10dfc899bf05facb8f52de715662c599fc887b4e
Dikendev <diken.dev@gmail.com>
2024-07-28 21:40:31 -0300

    add service to scrape the target site

--- src/app.service.ts
+++ src/app.service.ts
@@ -1,13 +1,17 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, Logger } from '@nestjs/common';
 import { Tesseract } from './external/ocr/tesseract';
 import { WordsIsFound } from './main/use-case/when-words-is-found/words-is-found';
 import { SuccessReadFile } from './main/use-case/when-success-read-image/success-read-file';
 import { CandidateStatusList } from './main/use-case/when-words-is-found/interfaces/candidate-status.interface';
 import { OcrDto } from './app.controller';
+import { Puppeteer } from './external/puppeteer/puppeteer';
 
 @Injectable()
 export class AppService {
-  constructor(private readonly tesseract: Tesseract) {}
+  constructor(
+    private readonly tesseract: Tesseract,
+    private readonly logger: Logger,
+  ) {}
 
   async recognizeFile(
     files: Array<Express.Multer.File>,
@@ -28,5 +32,12 @@ export class AppService {
       console.error('Error recognizing file:', error);
       return error;
     }
+  }
+
+  async navigate() {
+    const puppeteer = new Puppeteer(this.logger);
+    await puppeteer.init();
+    const url = process.env.SITE_SCRAPE_TARGET;
+    await puppeteer.navigate(url);
   }
 }
